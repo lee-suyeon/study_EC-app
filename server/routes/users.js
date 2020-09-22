21f28a46cdc7d2ff3389aea9b853e33d9a544caf
lee-suyeon <42996064+sroovy@users.noreply.github.com>
2020-09-22 14:56:58 +0900

    Add to Cart button

--- server/routes/users.js
+++ server/routes/users.js
@@ -9,62 +9,104 @@ const { auth } = require("../middleware/auth");
 //=================================
 
 router.get("/auth", auth, (req, res) => {
-    res.status(200).json({
-        _id: req.user._id,
-        isAdmin: req.user.role === 0 ? false : true,
-        isAuth: true,
-        email: req.user.email,
-        name: req.user.name,
-        lastname: req.user.lastname,
-        role: req.user.role,
-        image: req.user.image,
-    });
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
 });
 
 router.post("/register", (req, res) => {
 
-    const user = new User(req.body);
+  const user = new User(req.body);
 
-    user.save((err, doc) => {
-        if (err) return res.json({ success: false, err });
-        return res.status(200).json({
-            success: true
-        });
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true
     });
+  });
 });
 
 router.post("/login", (req, res) => {
-    User.findOne({ email: req.body.email }, (err, user) => {
-        if (!user)
-            return res.json({
-                loginSuccess: false,
-                message: "Auth failed, email not found"
-            });
-
-        user.comparePassword(req.body.password, (err, isMatch) => {
-            if (!isMatch)
-                return res.json({ loginSuccess: false, message: "Wrong password" });
-
-            user.generateToken((err, user) => {
-                if (err) return res.status(400).send(err);
-                res.cookie("w_authExp", user.tokenExp);
-                res
-                    .cookie("w_auth", user.token)
-                    .status(200)
-                    .json({
-                        loginSuccess: true, userId: user._id
-                    });
-            });
-        });
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: "Auth failed, email not found"
     });
+
+  user.comparePassword(req.body.password, (err, isMatch) => {
+    if (!isMatch)
+      return res.json({ loginSuccess: false, message: "Wrong password" });
+
+    user.generateToken((err, user) => {
+      if (err) return res.status(400).send(err);
+      res.cookie("w_authExp", user.tokenExp);
+      res
+        .cookie("w_auth", user.token)
+        .status(200)
+        .json({
+          loginSuccess: true, userId: user._id
+        });
+      });
+    });
+  });
 });
 
 router.get("/logout", auth, (req, res) => {
-    User.findOneAndUpdate({ _id: req.user._id }, { token: "", tokenExp: "" }, (err, doc) => {
-        if (err) return res.json({ success: false, err });
-        return res.status(200).send({
-            success: true
-        });
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "", tokenExp: "" }, (err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({ success: true });
+  });
+});
+
+router.post("/addToCart", auth, (req, res) => {
+  // 1. User Collection에 해당 유저의 정보를 가져온다. 
+  User.findOne({ _id: req.user._id },
+    (err, userInfo) => {
+      // 2. 가져온 정보에서 카트에다 넣으려하는 상품이 이미 들어 있는지 확인
+      let duplicate = false; 
+      userInfo.cart.forEach(item => {
+        if(item.id === req.body.productId){
+          duplicate = true; // 이미 user의 cart에 동일한 상품이 들어있다. 
+        }
+      });
+      // 3. 상품이 이미 있을 때 상품 개수 +1
+      if(duplicate) {
+        User.findOneAndDelete(
+          { _id: req.user._id, "cart.id": req.body.productId },
+          { $inc : { "cart.$.quantity": 1 }},
+          { new: true }, // 업데이트 된 userInfo정보를 받기 위한 옵션
+          (err, userInfo) => {
+            if(err) return res.status(200).json({ success: false, err});
+            res.status(200).send(userInfo.cart);
+          }
+        )
+      } else { // 4. 상품이 없을 때, 모든 정보 추가
+        User.findOneAndUpdate(
+          { _id: req.user._id },
+          {
+            $push: {
+              cart: {
+                id: req.body.productId,
+                quantity: 1,
+                date: Date.now()
+              }
+            }
+          },
+          { new: true },
+          (err, userInfo) => {
+            if(err) return res.status(400).json({ success: false, err });
+            res.status(200).send(userInfo.cart);
+          }
+        )
+      }
     });
 });
 
